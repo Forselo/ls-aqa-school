589eac65a7fc145a2fb87acb48ff627daabe15a1
Forselo <forelo@gmail.com>
2025-03-24 13:04:01 +0300

    Задание 5: Реализовать двухуровневую архитектуру тестового набора: -Сделать общий базовый класс для тестов TestBase, перенести вспомогательные методы в него -Сделать класс ApplicationManager и перенести вспомогательные методы в него, это рекомендуется делать при помощи рефакторинга в два шага, как описано в лекции "Заменяем наследование на делегирование". -Выделить из ApplicationManager специализированные классы-помощники NavigationHelper, GroupHelper и ContactHelper, перенести в них из ApplicationManager соответствующие вспомогательные методы. -Создать для этих вспомогательных классов общий базовый класс BaseHelper, перенести в него низкоуровневые вспомогательные методы (если такие есть).

--- addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbook/autotest/test/AddressBookTest.java
+++ addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbook/autotest/test/AddressBookTest.java
@@ -1,26 +1,27 @@
 package ru.ls.qa.school.addressbook.autotest.test;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.ls.qa.school.addressbook.autotest.app.ApplicationManager;
 import ru.ls.qa.school.addressbook.autotest.model.ContactData;
 
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class AddressBookTest {
 
+    private ApplicationManager app;
+
     @BeforeEach
     public void setUp() {
-        Configuration.browser = "chrome";
+        app = new ApplicationManager();
+        app.init();
     }
 
     @Test
     public void testAddNewContact() {
-        open("http://localhost/addressbook/");
-        login("admin", "secret");
-        goToAddNewContact();
+        app.login("admin", "secret");
+        app.getNavigationHelper().goToAddNewContact();
 
         ContactData contact = new ContactData(
                 "Семен",
@@ -31,36 +32,9 @@ public class AddressBookTest {
                 "7979543684797"
         );
 
-        fillContactForm(contact);
-        submitContactForm();
-        logout();
-    }
-
-    private void login(String username, String password) {
-        $(byName("user")).setValue(username);
-        $(byName("pass")).setValue(password);
-        $(byCssSelector("input[type='submit']")).click();
-    }
-
-    private void goToAddNewContact() {
-        $(byLinkText("add new")).click();
-    }
-
-    private void fillContactForm(ContactData contact) {
-        $(byName("firstname")).setValue(contact.getFirstName());
-        $(byName("middlename")).setValue(contact.getMiddleName());
-        $(byName("lastname")).setValue(contact.getLastName());
-        $(byName("address")).setValue(contact.getAddress());
-        $(byName("home")).setValue(contact.getHomePhone());
-        $(byName("mobile")).setValue(contact.getMobilePhone());
-    }
-
-    private void submitContactForm() {
-        $(byCssSelector("input[type='submit']")).click();
-    }
-
-    private void logout() {
-        $(byLinkText("Logout")).click();
+        app.getContactHelper().fillContactForm(contact);
+        app.getContactHelper().submitContactForm();
+        app.logout();
     }
 
     @AfterEach
